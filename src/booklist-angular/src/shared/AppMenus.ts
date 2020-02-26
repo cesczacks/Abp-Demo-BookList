6c0009a2dd88baad4f70d866fb1fe0c255d70881
qinhong.wang <qinhong.wang@accenture.com>
2020-02-26 19:20:42 +0800

    remove codes

--- src/booklist-angular/src/shared/AppMenus.ts
+++ src/booklist-angular/src/shared/AppMenus.ts
@@ -56,42 +56,4 @@ export class AppMenus {
             ]
         },
     ];
-    // old
-    // static Menus = [
-    //     // 首页
-    //     new MenuItem(
-    //         'HomePage',
-    //         '',
-    //         'anticon anticon-home',
-    //         '/app/home'
-    //     ), 
-    //     // 租户
-    //     new MenuItem(
-    //         'Tenants',
-    //         'Pages.Tenants',
-    //         'anticon anticon-team',
-    //         '/app/tenants',
-    //     ),
-    //     // 角色
-    //     new MenuItem(
-    //         'Roles',
-    //         'Pages.Roles',
-    //         'anticon anticon-safety',
-    //         '/app/roles',
-    //     ),
-    //     // 用户
-    //     new MenuItem(
-    //         'Users',
-    //         'Pages.Users',
-    //         'anticon anticon-user',
-    //         '/app/users',
-    //     ),
-    //     // 关于我们
-    //     new MenuItem(
-    //         'About',
-    //         '',
-    //         'anticon anticon-info-circle-o',
-    //         '/app/about',
-    //     ),
-    // ];
 }
